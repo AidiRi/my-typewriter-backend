ffc0ff803017235519f63adb237bfbfd5ea525ea
AidiRi <rivera.aidi@gmail.com>
2022-11-04 15:52:06 -0700

    changing req from GET to PUT; adding express as middleware, testing a json payload with Postman

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
 const app = express();
-
+// using express as middleware to parse json
+app.use(express.json());
 // .get() => 1. route path to listen for, 2. callback. 
 // Callback => 1. request object w/ req details, 2. response object
-app.get('/hello', (req, res) => {
-    res.send('Hello!');
+app.put('/hello', (req, res) => {
+    res.send(`Hello ${req.body.name}!`);
 });
 
 // listen() => 1. which port to listen on, 2. callback to call once server is listening
