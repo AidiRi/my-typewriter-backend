14704c467e1178e49121bd0eea497a7dbbb19488
AidiRi <rivera.aidi@gmail.com>
2022-11-07 14:57:44 -0800

    02_04: testing url params in Express

--- src/server.js
+++ src/server.js
@@ -9,6 +9,13 @@ app.put('/hello', (req, res) => {
     res.send(`Hello ${req.body.name}!`);
 });
 
+// :urlParamName key(s) specifies param wanted in route
+app.get('/hello/:name/goodbye/:otherName', (req, res) => {
+    const { name } = req.params; // destructuring params object
+    res.send(`Hello ${name}!!`);
+    console.log(req.params)
+});
+
 // listen() => 1. which port to listen on, 2. callback to call once server is listening
 app.listen(8000, () => {
     console.log('Server is listening on port 8000');
